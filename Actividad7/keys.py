b2735a9828f7b633d5555e37b20ec6aaf94bbd1d
José Alberto Rocha Munguía <163324182+AkJoss@users.noreply.github.com>
2025-10-01 15:46:06 -0600

    Actividad7

---
+++ Actividad7/keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+OWM_key = "60fb44151ca0b23ebde5de0a2427c413"
+flights_key = "c6a4ec4ff0msh6d76d907e03c67bp1ab10bjsnfc1b49127847"
+
+
+# In[ ]:
+
+
+
+
